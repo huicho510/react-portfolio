78d4ed9b65cf2b4f6f942b95164ba055c5409474
Luis Cervantes <luiscerv1229@gmail.com>
2020-08-24 17:10:41 -0700

    added components and Nav Bar

---
+++ react-portfolio/src/components/Nav.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <header>
+        <div className="menu-btn">
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+          <div className="btn-line"></div>
+        </div>
+
+        <nav className="menu">
+          <div className="menu-branding">
+            <div className="portrait"></div>
+          </div>
+          <ul className="menu-nav">
+            <li className="nav-item current">
+              <a href="./index.html" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="./about.html" className="nav-link">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="./work.html" className="nav-link">
+                My Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="./contact.html" className="nav-link">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Nav;
