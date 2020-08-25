e25b609b0547d48d48cb6fb768652231ca21ca97
Luis Cervantes <luiscerv1229@gmail.com>
2020-08-24 18:59:10 -0700

    converting old HTML/SASS potfolio to REACT has not been a success, managed to add BG and main body

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
-import Nav from './components/Nav'
+import Main from './components/Main'
 
 function App() {
   return (
     <div >
-      <Nav />
+      <Main />
     </div>
   );
 }
---
+++ react-portfolio/src/components/Main.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/Nav.css";
+import Nav from '../components/Nav'
+
+const Main = () => {
+  return (
+    <div className='body bg-img'>
+      <Nav />
+      <main className="home">
+        <h1 className="lg-heading">
+          Luis <span className="text-secondary">Cervantes</span>
+        </h1>
+        <h2 className="sm-heading">Web Developer, Programmer, & Designer</h2>
+        <div className="icons">
+          <a href="#!">
+            <i className="fab fa-facebook fa-2x"></i>
+          </a>
+          <a href="#!">
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+          <a href="#!">
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Main;
